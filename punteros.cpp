4f2e3f2d914835a9125add1bf57c6ec22317f38b
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-09-09 10:58:18 -0500

    final

--- punteros.cpp
+++ punteros.cpp
@@ -12,6 +12,8 @@ int main() {
     cout << apuntador << endl;
     cout << &a << endl;
     cout << a << endl;
-
+    cout << apuntador - a << endl;
+    int k[5] = {4, 7, 8, 90, 100};
+    cout << *(k+3) << endl;
     return 0;
 }
